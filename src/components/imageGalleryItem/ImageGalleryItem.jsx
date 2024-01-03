9c68fe984d910b115bbaee0852ed1ae34a98b179
Velms <78916991+Velms@users.noreply.github.com>
2024-01-03 13:49:19 +0200

    Update ImageGalleryItem.jsx

--- src/components/imageGalleryItem/ImageGalleryItem.jsx
+++ src/components/imageGalleryItem/ImageGalleryItem.jsx
@@ -4,7 +4,7 @@ import css from './ImageGalleryItem.module.css';
 export function ImageGalleryItem({ image, onClick }) {
   return (
     <li className={css.imageGalleryItem}>
-      <img id={image.id} src={image.webformatURL} alt={image.tags} data-large={image.largeImageURL} onClick={onClick} />
+      <img id={image.id} src={image.webformatURL} alt={image.tags} data-large={image.largeImageURL} onClick={(image.largeImageURL)=onClick > (image.tags)} />
     </li>
   );
 }
